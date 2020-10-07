671379b5d5c535960e6de4dc323652c0a6bf96f8
Swishyfishie <pclandreiuk@gmail.com>
2020-10-07 09:45:44 +0100

    reroll db with foreign key migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_083313) do
+ActiveRecord::Schema.define(version: 2020_10_07_084147) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "calories"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_10_07_083313) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "users"
 end
