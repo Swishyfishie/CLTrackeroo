1c5c607163fc8d242b274967fc0f2b776bd585a7
Swishyfishie <pclandreiuk@gmail.com>
2020-10-07 10:11:46 +0100

    add post item function

--- frontend/Index.js
+++ frontend/Index.js
@@ -17,4 +17,42 @@ class Item {
 
         `;
   }
+  postItem(){
+    // debugger
+    const item = {
+        name: document.querySelector("#itemName").value,
+        calories: document.querySelector("#calorieNumber").value
+    }
+
+    fetch(BASE_URL+"/items", {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(item => {
+        let items = document.querySelector("#items-container")
+        // items.innerHTML += `
+        // <div class="itemDetails" data-id="${item.id}"><p class="itemName">${item.name}: </p>  <p class="itemCalories">${item.calories}</p> 
+        // <button class="deleteItem">Delete</button><button id="edit-${this.id}" class="editItem">Edit</button></div>
+        // `
+        items.innerHTML += `
+        <div class="itemDetails" data-id="${item.id}">
+            <p class="itemName">${item.name}:</p>  <p class="itemCalories">${item.calories}</p> 
+            <button id="delete-${item.id}" class="deleteItem">Delete</button>
+            <button id="edit-${item.id}" class="editItem">Edit</button>
+        </div>
+        `
+        addDeleteAndEditListeners()
+        App.allItems.push(new Item(item))
+        // debugger
+        App.getTotalCals()
+    })
+    document.querySelector("#itemName").value = ''
+    document.querySelector("#calorieNumber").value = ''
+
+}
 }
