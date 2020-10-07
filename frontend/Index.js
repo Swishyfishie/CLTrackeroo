034bf133f1608d260557995665e4111087321a5f
Swishyfishie <pclandreiuk@gmail.com>
2020-10-07 10:11:19 +0100

    add render in Item class

--- frontend/Index.js
+++ frontend/Index.js
@@ -1,8 +1,20 @@
-class Item{
-    constructor(obj){
-        this.id = obj.id
-        this.name = obj.name 
-        this.calories = obj.calories
+class Item {
+  constructor(obj) {
+    this.id = obj.id;
+    this.name = obj.name;
+    this.calories = obj.calories;
+  }
 
-    }    
+  render() {
+    // generate dynamic ids for the buttons "delete-${e.id}"
+
+    return `
+        <div class="itemDetails" data-id="${this.id}">
+            <p class="itemName">${this.name}:</p>  <p class="itemCalories">${this.calories}</p> 
+            <button id="delete-${this.id}" class="deleteItem">Delete</button>
+            <button id="edit-${this.id}" class="editItem">Edit</button>
+        </div>
+
+        `;
+  }
 }
